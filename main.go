88f1eb613d0c87b25e445d1b1694690bae8d7bc4
rpadliwinata <rpadliwinata@gmail.com>
2023-08-03 12:39:30 +0700

    update

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package iam
 
-import "fmt"
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"encoding/json"
+)
 
-func Example() {
-	fmt.Println("example")
+func Decode(client string, token string) (map[string]interface{}, error) {
+	data := map[string]interface{} {
+		"client": client,
+		"token": token,
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.Post("https://fastapiiam-1-i2172913.deta.app/decode", "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return nil, err
+	}
+
+	responseData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var responseMap map[string]interface{}
+	err = json.Unmarshal(responseData, &responseMap)
+	if err != nil {
+		return nil, err
+	}
+
+	return responseMap, nil
 }
